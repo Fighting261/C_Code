7b8a20e1717e582842b48776997a02bebfc682e6
Fighting261 <1227873949@qq.com>
2020-03-18 18:13:57 +0800

    输出三个整数中的最大值

---
+++ class_3_18/class_3_18/class.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int a, b, c,max;
+	printf("请输入三个整数\n");
+	scanf("%d%d%d", &a, &b, &c);
+
+	    max = a;
+	if (a < b)
+		max = b;
+    if (b < c)
+		max=c;
+	printf("最大的数字是%d\n", max);
+
+
+}
