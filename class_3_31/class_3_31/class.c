c446347105978797f1b3728a4d5bbe1241671741
Fighting261 <1227873949@qq.com>
2020-03-31 21:28:40 +0800

    1_ 错误代码 2_ 算数转换 3_ 前置++和后置++

---
+++ class_3_31/class_3_31/class.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+////错误代码
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	int ret = (++i) + (++i) + (++i);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+////算术转换
+//#include <stdio.h>
+//int i;
+//int main()
+//{
+//	i--;
+//	if (i > sizeof(i))
+//	{
+//		printf(">\n");
+//	}
+//	else
+//	{
+//		printf("<\n");
+//	}
+//	return 0;
+//}
+
+//前置++和后置++
+#include <stdio.h>
+int main()
+{
+	int a, b, c;
+	a = 5;
+	c = ++a;
+	b = ++c, c++, ++a, a++;
+	b += a++ + c;
+	printf("a = %d b = %d c = %d\n:", a, b, c);
+	return 0;
+}
